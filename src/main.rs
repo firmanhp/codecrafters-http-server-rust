d4bdecf36c70c289be02041121ce207a52085793
Firman Hadi P <ngiks@ngiks-mbook.local>
2024-06-08 21:41:28 +0800

    introduce request structure, respond on specific paths.

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,60 @@
-use std::net::{TcpListener, TcpStream};
-use std::io::Write;
+use std::io::ErrorKind;
+use std::io::Read;
 use std::io::Result;
+use std::io::Write;
+use std::net::{TcpListener, TcpStream};
+
+mod request;
+use request::HttpRequest;
 
 fn handle_client(mut stream: TcpStream) -> Result<()> {
-    stream.write(b"HTTP/1.1 200 OK\r\n\r\n")?;
-    Ok(())
+    let mut total_buffer: Vec<u8> = Vec::new();
+    let mut ask_for_byte: bool = true;
+    let stop_condition: &[u8] = b"\r\n\r\n";
+    while ask_for_byte {
+        let mut single_byte: [u8; 1] = [0];
+        let result = stream.read(&mut single_byte);
+        if let Err(error) = result {
+            match error.kind() {
+                ErrorKind::Interrupted => continue,
+                _ => return Err(error),
+            }
+        }
+        if let Ok(n) = result {
+            if n == 0 {
+                continue;
+            }
+        }
+
+        total_buffer.push(single_byte[0]);
+        // Check for stop condition.
+        let needs_to_stop: bool = single_byte[0] == stop_condition[stop_condition.len() - 1]
+            && total_buffer.len() >= stop_condition.len()
+            && &total_buffer[(total_buffer.len() - stop_condition.len())..] == stop_condition;
+        ask_for_byte = !needs_to_stop;
+        // println!("current buffer: {}", String::from_utf8_lossy(total_buffer.as_slice()));
+    }
+
+    process(stream, &total_buffer.as_slice())
 }
 
+fn process(mut stream: TcpStream, buf: &[u8]) -> Result<()> {
+    println!("buffer: {}", String::from_utf8_lossy(&buf));
+    let request = HttpRequest::from_bytes(buf)?;
+    println!("request: {}", request);
+
+    if &request.path == "/" {
+        stream.write(b"HTTP/1.1 200 OK\r\n\r\n")?;
+    } else {
+        stream.write(b"HTTP/1.1 404 Not Found\r\n\r\n")?;
+    }
+    return Ok(());
+}
 
 fn main() -> Result<()> {
     println!("Logs from your program will appear here!");
 
-    let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
-    
+    let listener: TcpListener = TcpListener::bind("127.0.0.1:4221").unwrap();
     for stream in listener.incoming() {
         match stream {
             Ok(_stream) => {
---
+++ src/request.rs
@@ -0,0 +1,136 @@
+use std::fmt;
+use std::io::Error;
+use std::io::ErrorKind;
+use std::io::Result;
+
+use itertools::Itertools;
+
+#[derive(Debug)]
+pub enum HttpRequestType {
+    Get,
+    Post,
+    Put,
+    Delete,
+    // may add more here...
+}
+
+impl fmt::Display for HttpRequestType {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "HttpRequestType::{:?}", self)
+    }
+}
+
+impl HttpRequestType {
+    fn from_str(type_str: &str) -> Result<HttpRequestType> {
+        match type_str {
+            "GET" => Ok(HttpRequestType::Get),
+            "POST" => Ok(HttpRequestType::Post),
+            "PUT" => Ok(HttpRequestType::Put),
+            "DELETE" => Ok(HttpRequestType::Delete),
+            _ => Err(Error::new(
+                ErrorKind::InvalidData,
+                format!("Request line is invalid: {}", type_str),
+            )),
+        }
+    }
+}
+
+#[derive(Default)]
+pub struct HttpRequestHeader {
+    pub host: Option<String>,
+    pub user_agent: Option<String>,
+    pub accept: Option<String>,
+}
+
+impl fmt::Display for HttpRequestHeader {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "HttpRequestHeader{{")?;
+        write!(f, "Host: {:?}, ", self.host)?;
+        write!(f, "User-Agent: {:?}, ", self.user_agent)?;
+        write!(f, "Accept: {:?}, ", self.accept)?;
+        write!(f, "}}")
+    }
+}
+
+impl HttpRequestHeader {
+    fn from_buffer_lines(lines: &[&str]) -> HttpRequestHeader {
+        let mut header: HttpRequestHeader = Default::default();
+        for line in lines {
+            // We reached the end here i.e. \r\n\r\n
+            if *line == "" {
+                break;
+            }
+            let key_value = (*line).splitn(2, ": ").collect_vec();
+            if key_value.len() != 2 {
+                println!("WARNING: ignoring header {}", *line);
+                continue;
+            }
+            match key_value[0] {
+                "Host" => {
+                    header.host = Some(String::from(key_value[1]));
+                }
+                "User-Agent" => header.user_agent = Some(String::from(key_value[1])),
+                "Accept" => header.accept = Some(String::from(key_value[1])),
+                _ => {
+                    println!("WARNING: unknown header key: {}", key_value[0]);
+                }
+            }
+        }
+
+        header
+    }
+}
+
+pub struct HttpRequest {
+    pub req_type: HttpRequestType,
+    pub path: String,
+    pub header: HttpRequestHeader,
+    pub body: Option<String>,
+}
+
+impl fmt::Display for HttpRequest {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "HttpRequest{{")?;
+        write!(f, "Type: {}, ", self.req_type)?;
+        write!(f, "Path: {}, ", self.path)?;
+        write!(f, "Header: {}, ", self.header)?;
+        write!(f, "Body: {:?}, ", self.body)?;
+        write!(f, "}}")
+    }
+}
+
+impl HttpRequest {
+    pub fn from_bytes(buf: &[u8]) -> Result<HttpRequest> {
+        let buf_str = String::from_utf8_lossy(buf);
+        let mut request_type: HttpRequestType = HttpRequestType::Get;
+        let mut path: String = String::from("");
+
+        let delimiter: &str = "\r\n";
+        let req_lines = buf_str.split(delimiter).collect_vec();
+
+        if req_lines.len() == 0 {
+            return Err(Error::new(ErrorKind::InvalidData, "Unspecified request"));
+        }
+
+        // First request line
+        if req_lines.len() >= 1 {
+            let req_line = req_lines[0].split(" ").collect_vec();
+            if req_line.len() != 3 {
+                return Err(Error::new(
+                    ErrorKind::InvalidData,
+                    "Request line is invalid",
+                ));
+            }
+            request_type = HttpRequestType::from_str(req_line[0])?;
+            path = String::from(req_line[1]);
+            // ignore HTTP version from now
+        }
+
+        Ok(HttpRequest {
+            req_type: request_type,
+            path: path,
+            header: HttpRequestHeader::from_buffer_lines(&req_lines[2..]),
+            body: None,
+        })
+    }
+}
